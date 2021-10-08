68e1d20f4599a777e1ad8dc3461c017bf379601d
chasefarmer2808 <chasefarmer2808@ufl.edu>
2021-10-08 08:22:42 -0400

    fade animation

--- src/HitComponent.jsx
+++ src/HitComponent.jsx
@@ -13,7 +13,9 @@ export const HitComponent = () => {
     return (
         <section className="hit-component-root">
             <h2>Hit Count</h2>
-            {hits > 0 ? <p aria-label={hits}><strong>{hits}</strong></p> : <p>Click the button to get the latest hits</p>}
+            {hits > 0 
+                ? <p className="fade-in-down" aria-label={hits}><strong>{hits}</strong></p> 
+                : <p>Click the button to get the latest hits</p>}
             <button onClick={handleHit} aria-label="Hit the counter API.">Hit</button>
         </section>
     )
